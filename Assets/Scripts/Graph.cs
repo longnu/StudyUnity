cc9672c274818ec650dc99aa0d4d773a55db571c
longnu <83560500+longnu@users.noreply.github.com>
2024-01-04 09:32:37 +0900

    feat : 2.2 Basics - Mathematical Surfaces - 3. Adding Another Dimension

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,6 +8,9 @@ public class Graph : MonoBehaviour
     Transform pointPrefab;
     [SerializeField, Range(10, 100)]
     int resolution = 10;
+
+    [SerializeField] 
+    FunctionLibrary.FunctionName function;
     
     Transform[] points;
     
@@ -19,18 +22,28 @@ public class Graph : MonoBehaviour
         
     }
 
-    void Awake () {
+    void Awake ()
+    {
+        
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
         
         
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++) {
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x= 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+            
             Transform point = points[i] =Instantiate(pointPrefab);
             
             point.SetParent(transform, false);
-            position.x = (i + 0.5f) * step - 1f;
+            //position.x = ( i + 0.5f) * step - 1f;
+            position.x = ( x + 0.5f) * step - 1f;
+            position.z = ( z + 0.5f) * step - 1f;
             
             point.localPosition = position;
             point.localScale = scale;
@@ -42,12 +55,14 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
         
